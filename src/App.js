07bf3466d01fcb14a0d28e11b23e287a1c9c849f
Robert Payne <rp1242@gmail.com>
2018-12-28 12:30:44 -0600

    revert test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>React Bejeweled initializeLevel branch</p>
+        <p>React Bejeweled</p>
         <Game />
       </div>
     );
